63a25fe4327190ec9498dc6bac4080246c841077
Hooray <304327508@qq.com>
2026-04-14 03:22:32 +0800

    feat: 更新依赖和示例

--- example/vue/vite.config.ts
+++ example/vue/vite.config.ts
@@ -2,7 +2,7 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import Archiver from 'vite-plugin-archiver'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), Archiver()],
 })
